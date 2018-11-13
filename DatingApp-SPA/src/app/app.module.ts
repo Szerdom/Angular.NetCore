604afe34b8875c38f0c2d361ce550af75c06e64d
Dominik Szeremeta <szeremetadom@gmail.com>
2018-11-13 17:17:26 +0100

    User info tabs

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { NgxGalleryModule } from 'ngx-gallery';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
@@ -22,6 +23,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export function tokenGetter() {
   const helper = localStorage.getItem('token');
@@ -38,14 +42,17 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
       FormsModule,
       HttpClientModule,
       BsDropdownModule.forRoot(),
+      TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
       JwtModule.forRoot({
         config: {
           tokenGetter: tokenGetter,
@@ -59,7 +66,9 @@ export function tokenGetter() {
       ErrorInterceptorProvider,
       AlertifyService,
       AuthGuard,
-      UserService
+      UserService,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -5,12 +5,15 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard], children: [
-    { path: 'members', component: MemberListComponent},
-    { path: 'members/:id', component: MemberDetailComponent},
+    { path: 'members', component: MemberListComponent, resolve: {users: MemberDetailResolver}},
+    { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+    { path: 'member/edit', component: MemberEditComponent},
     { path: 'messages', component: MessagesComponent },
     { path: 'lists', component: ListsComponent },
   ]},
